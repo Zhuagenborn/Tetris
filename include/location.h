2da5391af3db628cc2e992bfa13d81558898e133
Chenzs108 <chenzs108@outlook.com>
2023-05-01 22:49:28 +0000

    Feat: Add a movable interface and coordinate

---
+++ include/location.h
@@ -0,0 +1,45 @@
+/**
+ * @file location.h
+ * @brief The movable object and coordinate.
+ *
+ * @author Zhenshuo Chen (chenzs108@outlook.com)
+ * @par GitHub
+ * https://github.com/Zhuagenborn
+ * @version 1.0
+ * @date 2023-04-28
+ */
+
+#pragma once
+
+#include <cstddef>
+
+/**
+ * A coordinate.
+ */
+struct Point {
+    constexpr Point(const std::size_t x = 0, const std::size_t y = 0) noexcept :
+        x {x}, y {y} {}
+
+    std::size_t x;
+    std::size_t y;
+};
+
+/**
+ * @interface Locatable
+ */
+class Locatable {
+public:
+    virtual Point GetPosition() const noexcept = 0;
+
+    virtual ~Locatable() noexcept = default;
+};
+
+/**
+ * @interface Movable
+ */
+class Movable : public Locatable {
+public:
+    virtual void SetPosition(Point) noexcept = 0;
+
+    virtual ~Movable() noexcept = default;
+};
